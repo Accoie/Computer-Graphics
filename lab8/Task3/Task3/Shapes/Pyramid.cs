6b2f0a78c8c22b4152ce4d63f7383de3644fe43b
Accoie <sypherjensqr@gmail.com>
2026-05-19 10:16:25 +0300

    маленькие правки

--- lab8/Task3/Task3/Shapes/Pyramid.cs
+++ lab8/Task3/Task3/Shapes/Pyramid.cs
@@ -10,7 +10,7 @@ public class Pyramid
         new(new TorusData(new Vector3(0, -0.8f, 0), 0.9f, 0.3f), new Vector3(1, 0, 0)),
         new(new TorusData(new Vector3(0, -0.3f, 0), 0.7f, 0.25f), new Vector3(0, 1, 0)),
         new(new TorusData(new Vector3(0, 0.1f, 0), 0.5f, 0.2f), new Vector3(0, 0, 1)),
-        new(new TorusData(new Vector3(0, 0.4f, 0), 0.3f, 0.15f), new Vector3(1, 1, 0)),
+        new(new TorusData(new Vector3(0, 0.4f, 0), 0.3f, 0.15f), new Vector3(0.95f, 0.08f, 0.73f)),
     ];
 
     public IReadOnlyList<Torus> Toruses => _toruses;
